ced2afb4d5e0391e549caae2d62a1228d0c62e3b
Sai Prashanth Volete <saivoleteprashanth@gmail.com>
2023-04-12 18:37:01 -0500

    Post method is added in Drink.js

--- controllers/Drink.js
+++ controllers/Drink.js
@@ -1,5 +1,6 @@
 var Drink = require('../models/Drink');
 // List of all Drinks
+
 exports.Drink_list = function(req, res) {
  res.send('NOT IMPLEMENTED: Drink list');
 };
@@ -18,9 +19,9 @@ exports.Drink_delete = function(req, res) {
 // Handle Drink update form on PUT.
 exports.Drink_update_put = function(req, res) {
  res.send('NOT IMPLEMENTED: Drink update PUT' + req.params.id);
-};
+};// List of all Drinks
 
-// List of all Drinks
+// List of all Drink
 exports.Drink_list = async function(req, res) {
     try{
     theDrink = await Drink.find();
@@ -31,3 +32,40 @@ exports.Drink_list = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
    };
+
+
+
+// VIEWS
+// Handle a show all view
+exports.Drink_view_all_Page = async function(req, res) {
+    try{
+    theDrink = await Drink.find();
+    res.render('Drinks', { title: 'Drink Search Results', results: theDrink });
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+   };
+
+
+   // Handle Costume create on POST.
+exports.Drink_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new Drink();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"Drink_type":"goat", "cost":12, "size":"large"}
+    document.drink_type = req.body.drink_type;
+    document.drink_size = req.body.drink_size;
+    document.drink_cost = req.body.drink_cost;
+    try{
+    let result = await document.save();
+    res.send(result);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+};
--- routes/Drinks.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('Drinks', { title: 'Search Results of Drinks' });
-});
-
-module.exports = router;
